02e4f1be31e8abca39cb9f541b2fea608ae11a1d
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-02 15:50:45 +0900

    feat: 4단계 input 로직 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,32 +1,53 @@
 package view;
 
-import model.LadderHeight;
-import model.LadderWidth;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.Scanner;
+import model.player.Players;
 
 public class InputView {
 
   private static final Scanner scanner = new Scanner(System.in);
-  private static final String INPUT_EXCEPTION_MESSAGE = "올바른 입력 형식이 아닙니다. 다시 입력해주세요.";
+  public static final String INPUT_EXCEPTION_MESSAGE = "올바른 입력 형식이 아닙니다. 다시 입력해주세요.";
+  private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+  private static final String EXECUTION_GOAL_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+  private static final String EXECUTION_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+  private static final String PLAYER_FOR_RESULT_MESSAGE = "결과를 보고 싶은 사람은?";
+  private static final String INPUT_DELIMITER = ",";
 
+  public int inputLadderHeight() {
+    System.out.println("\n" + LADDER_HEIGHT_MESSAGE);
+    String rawLadderHeight = scanner.nextLine();
+    return parseInt(rawLadderHeight);
+  }
 
-  public LadderHeight inputLadderHeight() {
+  private int parseInt(String rawLadderHeight) {
     try {
-      System.out.println("사다리의 높이는 몇 개인가요?");
-      return new LadderHeight(Integer.parseInt(scanner.nextLine()));
-    } catch (IllegalArgumentException e) {
-      System.out.println(INPUT_EXCEPTION_MESSAGE);
-      return inputLadderHeight();
+      return Integer.parseInt(rawLadderHeight);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException(INPUT_EXCEPTION_MESSAGE);
     }
   }
 
-  public LadderWidth inputLadderWidth() {
-    try {
-      System.out.println("사다리 넓이는 몇 개인가요?");
-      return new LadderWidth(Integer.parseInt(scanner.nextLine()));
-    } catch (IllegalArgumentException e) {
-      System.out.println("INPUT_EXCEPTION_MESSAGE");
-      return inputLadderWidth();
-    }
+  public List<String> inputPlayers() {
+    System.out.println("\n" +EXECUTION_NAME_MESSAGE);
+    String rawPlayerName = scanner.nextLine();
+    return Arrays.stream(rawPlayerName.split(INPUT_DELIMITER))
+        .map(String::trim)
+        .collect(Collectors.toList());
+  }
+
+  public List<String> inputGoals(Players players) {
+    System.out.println("\n" + EXECUTION_GOAL_MESSAGE);
+    String rawGoals = scanner.nextLine();
+    return Arrays.stream(rawGoals.split(INPUT_DELIMITER))
+        .map(String::trim)
+        .collect(Collectors.toList());
+  }
+
+  public String inputPlayerForResult() {
+    System.out.println("\n" + PLAYER_FOR_RESULT_MESSAGE);
+    return scanner.nextLine();
   }
 }
