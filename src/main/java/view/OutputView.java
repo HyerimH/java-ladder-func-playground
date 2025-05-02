17e5f240607cf0ade31d11d1e0ffa24d8459439a
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-02 16:01:53 +0900

    feat: 4단계 출력 로직 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,23 +1,89 @@
 package view;
 
-import model.Ladder;
-import model.Status;
+import java.util.List;
+import model.goal.Goal;
+import model.goal.Goals;
+import model.ladder.Ladder;
+import model.player.Player;
+import model.player.Players;
+import model.ladder.Point;
 import java.util.Map;
 
 public class OutputView {
-  public void printLadder(Ladder ladder) {
+
+  private static final int MAXIMUM_PLAYER_NAME_SPACE = 6;
+  private static final String EXECUTION_RESULT = "실행 결과";
+  private static final String CONNECTED_LINE="-----|";
+  private static final String DISCONNECTED_LINE="     |";
+  private static final String EDGE_OF_POINT="|";
+
+  public void printLadder(Ladder ladder, Players players, Goals goals) {
+    printPlayerNamers(players);
+    printLadderLines(ladder);
+    printGoals(goals);
+  }
+
+  private void printPlayerNamers(Players players) {
+    for (Player player : players.getPlayers()) {
+      System.out.printf("%-" + MAXIMUM_PLAYER_NAME_SPACE + "s", player.getName().getName());
+    }
+    System.out.println();
+  }
+
+  private void printLadderLines(Ladder ladder) {
     for (var line : ladder.getLines()) {
-      System.out.print("|");
-      for (Status status : line.getPoints()) {
-        System.out.print(status.getStatus() ? "----|" : "    |");
-      }
-      System.out.println();
+      printLine(line.getPoints());
     }
   }
 
-  public void printResults(Map<Integer, Integer> results) {
+  private void printLine(List<Point> points) {
+    System.out.print("  "+EDGE_OF_POINT);
+    for (var point : points) {
+      printPoint(point);
+    }
+    System.out.println();
+  }
+
+  private void printPoint(Point point) {
+    if(point.isConnected()){
+      System.out.print(CONNECTED_LINE);
+      return;
+    }
+    System.out.print(DISCONNECTED_LINE);
+  }
+
+  private void printGoals(Goals goals) {
+    System.out.print("  ");
+    for (Goal goal : goals.getGoals()) {
+      System.out.printf("%-" + MAXIMUM_PLAYER_NAME_SPACE + "s", goal.getGoal());
+    }
+    System.out.println();
+  }
+
+  public void printResult(String input, Players players, Map<Player, Goal> results) {
+    System.out.println("\n" + EXECUTION_RESULT);
+    if (input.equals("all")) {
+      printAllResults(results);
+      return;
+    }
+    printSingleResult(input, players, results);
+  }
+
+  private void printAllResults(Map<Player, Goal> results) {
     for (var entry : results.entrySet()) {
-      System.out.println(entry.getKey() + " -> " + entry.getValue());
+      System.out.println(entry.getKey().getName().getName() + " : " + entry.getValue().getGoal());
+    }
+  }
+
+  private void printSingleResult(String name, Players players, Map<Player, Goal> results) {
+    for (Player player : players.getPlayers()) {
+      printPlayerResult(name, player, results);
+    }
+  }
+
+  private void printPlayerResult(String name, Player player, Map<Player, Goal> results) {
+    if (player.getName().getName().equals(name)) {
+      System.out.println(results.get(player).getGoal());
     }
   }
 }
